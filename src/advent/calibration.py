5e613e6b34c850278bb891c5f334922be62efadc
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2023-12-02 02:39:41 +0530

    Refactor parser implementation

--- src/advent/calibration.py
+++ src/advent/calibration.py
@@ -1,15 +1,21 @@
 from typing import List
 
 
-def process_calib_values(calib_values: List) -> int:
-    return sum(calib_values)
-
-
 def parse_by_digit(doc_line: str) -> int:
-    l_idx_gen, r_idx_gen = range(0, len(doc_line), 1), range(len(doc_line) - 1, -1, -1)
+    digits = "0123456789"
+    l_digit, r_digit = None, None
 
-    l_digit = first_number_digit(doc_line, l_idx_gen)
-    r_digit = first_number_digit(doc_line, r_idx_gen)
+    for char in doc_line:
+        if char in digits:
+            if l_digit is None:
+                l_digit = char
+            if r_digit is None:
+                r_digit = char
+
+    if l_digit is None:
+        # Warning: input doesn't have digits, will set value to zero as default.
+        l_digit, r_digit = "0", "0"
+
     calib_value = int(l_digit + r_digit)
     return calib_value
 
@@ -18,12 +24,5 @@ def parse_by_digit_or_word(doc_line: str) -> int:
     pass
 
 
-def first_number_digit(line: str, range_gen) -> [int, None]:
-    digits = "0123456789"
-
-    for idx in range_gen:
-        if line[idx] in digits:
-            return line[idx]
-
-    # Warn: This return value can cause issues if the input data does not have digits.
-    return 0
+def process_calib_values(calib_values: List) -> int:
+    return sum(calib_values)
--- src/day_01_part_01.py
+++ src/day_01_part_01.py
@@ -1,27 +1,16 @@
 from pathlib import Path
-from typing import List
 
 from advent.calibration import process_calib_values, parse_by_digit
 from advent.utils import read_inputs
 
 
-def compute_calib_values(calib_doc_lines: List) -> List:
-    calib_values = []
-
-    for line in calib_doc_lines:
-        calib_value = parse_by_digit(line)
-        calib_values.append(calib_value)
-
-    return calib_values
-
-
-def driver_main():
-    inputs_path = Path("../inputs/aoc-2023-d1-p1-input.txt")
-    calib_doc_lines = read_inputs(inputs_path)
-    calib_values = compute_calib_values(calib_doc_lines)
-    output_data = process_calib_values(calib_values)
-    print(output_data)
+def solve_calibration(input_pathstr: str):
+    calib_doc_path = Path(input_pathstr)
+    calib_doc_lines = read_inputs(calib_doc_path)
+    calib_values = [parse_by_digit(line) for line in calib_doc_lines]
+    calib_output_data = process_calib_values(calib_values)
+    print(calib_output_data)
 
 
 if __name__ == "__main__":
-    driver_main()
+    solve_calibration("../inputs/aoc-2023-d1-p1-input.txt")
